d94399bbf5e19aaa4b54536dff6627a553749fde
MHTON1 <1733651640@qq.com>
2020-12-03 16:23:57 +0800

    Site updated: 2020-12-03 16:23:57

--- dist/music.js
+++ dist/music.js
@@ -3,33 +3,33 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [{
-            name: "平凡之路",
-            artist: '朴树',
-            url: 'http://www.ytmp3.cn/down/59211.mp3',
+            name: "Variations on the Kanon By Pachelbel",
+            artist: 'George Winston',
+            url: 'http://music.163.com/song/media/outer/url?id=1374348.MP3',
             cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
         },
         {
-            name: ' 夏を待つセイル(帆)のように ',
+            name: 'DANDAN心魅かれてく（渐渐被你吸引）',
             artist: 'Zard',
-            url: 'http://music.163.com/song?id=677325&userid=104456876',
+            url: 'http://music.163.com/song/media/outer/url?id=477331768.mp3',
             cover: '',
         },
         {
             name: '能看见海的城市',
             artist: '久石让',
-            url: 'http://music.163.com/song?id=28457555&userid=104456876',
+            url: 'http://music.163.com/song/media/outer/url?id=28457555.mp3',
             cover: '',
         },
         {
             name: 'Normal',
             artist: 'Sasha Sloan',
-            url: 'http://music.163.com/song?id=534544522&userid=104456876',
+            url: 'http://music.163.com/song/media/outer/url?id=534544522.mp3',
             cover: '',
         },
         {
             name: '我喜欢',
             artist: '上海彩虹室内合唱团',
-            url: 'http://music.163.com/song?id=464647435&userid=104456876',
+            url: 'http://music.163.com/song/media/outer/url?id=464647435.mp3,
             cover: '',
         }
     ]
